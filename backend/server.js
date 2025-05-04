d37af129f75c54f1a598975c4e3493cf61d68231
Josh Rime <joshrime1990@icloud.com>
2025-05-04 02:19:26 -0500

    register user controller with jwt

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
